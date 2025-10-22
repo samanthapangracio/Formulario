84efc212cbefa6b013e158e9bb7d21ed957f4230
samantha <samantha.pangracio@escola.pr.gov.br>
2025-10-22 07:36:46 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
+ const texto = document.querySelector("#entrada-de-texto").value;
+ const campoResultado = document.querySelector("#resultado-palavrachave");
+ campoResultado.textContent = texto;
 } 
